7610f2dfbadf9ea2ce752c3b56de135b572dc6aa
Koubae <58447627+Koubae@users.noreply.github.com>
2022-11-28 15:57:44 +0100

    python scripts misc.py

--- scripts/misc.py
+++ scripts/misc.py
@@ -1,14 +1,16 @@
 """Collection of scripts / recipes
 """
 
-_byte_array = bytearray(b'\xFF\x0F')
-print(_byte_array)
+def byte_array_functions():
+    _byte_array = bytearray(b'\xFF\x0F')
+    print(_byte_array)
 
-_byte_array[1] = 255
-print(_byte_array)
-_byte_array.append(128)
-print(_byte_array)
+    _byte_array[1] = 255
+    print(_byte_array)
+    _byte_array.append(128)
+    print(_byte_array)
 
+# byte_array_functions()
 
 def string_with_padding():
     """How to add padding to a f-string"""
@@ -19,4 +21,47 @@ def string_with_padding():
     print(f'Padding = {padding}')
     print(f'Message Padded = {message_padded}')
 
-string_with_padding()
+# string_with_padding()
+
+
+
+def struct_package():
+    import struct
+    #payload_size = struct.calcsize('>L')
+    #print(payload_size)
+    print(struct.calcsize('>l'))
+    print(struct.calcsize('>L'))
+    print(struct.calcsize('=L'))
+
+    print(struct.calcsize('=f'))
+    print(struct.calcsize('=d'))
+    print(struct.calcsize('>f'))
+    print(struct.calcsize('>d'))
+
+    print(struct.calcsize('<f'))
+    print(struct.calcsize('<d'))
+
+    print(struct.calcsize('@l'))
+    print(struct.calcsize('@L'))
+    print(struct.calcsize('@f'))
+    print(struct.calcsize('@d'))
+
+
+    message = "Hello world".encode('utf-8')
+    message = message[:struct.calcsize(">L")]
+
+    message_unpacked = struct.unpack(">L", message)
+    print(message_unpacked)
+
+    packed = struct.pack('i', 4)
+    print(packed)
+    INT_MAX = 2147483647
+    packed = struct.pack('i', INT_MAX)
+    print(packed)
+
+    packed = struct.pack('i', INT_MAX)
+    print(packed)
+
+
+struct_package()
+
