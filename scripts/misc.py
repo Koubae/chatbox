546a1b4f7527c7acfb0d9d855d4785357ea54431
Koubae <58447627+Koubae@users.noreply.github.com>
2022-11-28 12:52:18 +0100

    python scripts misc.py

--- scripts/misc.py
+++ scripts/misc.py
@@ -8,3 +8,15 @@ _byte_array[1] = 255
 print(_byte_array)
 _byte_array.append(128)
 print(_byte_array)
+
+
+def string_with_padding():
+    """How to add padding to a f-string"""
+    message = "Hello World"
+    padding = 10
+    message_padded = f'{len(message):<{padding}}{message}'
+    print(f'Message = {message}')
+    print(f'Padding = {padding}')
+    print(f'Message Padded = {message_padded}')
+
+string_with_padding()
