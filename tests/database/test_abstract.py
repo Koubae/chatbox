94d2450f77b8c8c37ce13249031369caa0d66083
Koubae <58447627+Koubae@users.noreply.github.com>
2023-04-30 15:46:53 +0200

    ModelBase --> Repository Base renamed

--- tests/database/test_abstract.py
+++ tests/database/test_abstract.py
@@ -33,56 +33,56 @@ class RepositoryConcrete(RepositoryBase):
 
 	def _build_object(self, data: Item) -> t.Any: ...
 
-class TestModelBase(BaseRunner):
+class TestRepositoryBase(BaseRunner):
 	@pytest.mark.db_abstract
 	@pytest.mark.sqlite
 	@pytest.mark.database
 	def test_subclass_abstract_properties_implementation(self):
-		model = RepositoryConcrete(self.db)
+		repository = RepositoryConcrete(self.db)
 
-		assert model._table_name == "user"
+		assert repository._table_name == "user"
 
 	@pytest.mark.db_abstract
 	@pytest.mark.sqlite
 	@pytest.mark.database
 	def test_subclass_crud_operations(self):
-		model = RepositoryConcrete(self.db)
+		repository = RepositoryConcrete(self.db)
 
 		# CREATE
-		model.create_user("user1", "1234")
-		assert model.created is 1
+		repository.create_user("user1", "1234")
+		assert repository.created is 1
 
 		# CREATE Many
 		total_users = 5
-		model.create_users([{"username": f"user{i}", "password": "1234"} for i in range(2, total_users + 2)])
-		assert model.created is total_users
+		repository.create_users([{"username": f"user{i}", "password": "1234"} for i in range(2, total_users + 2)])
+		assert repository.created is total_users
 
 		# READ
-		user = model.get_user_by_name("user1")
+		user = repository.get_user_by_name("user1")
 		assert user["username"] == "user1" and user["id"] is 1 and user["password"] == "1234"
 
 		# LIST
-		users = model.list_users()
+		users = repository.list_users()
 		assert [user["id"] for user in users] == list(range(1, 7))
 
 		# UPDATE
 		username_old = "user1"
 		username_new = "user1-new"
-		user_before_update = model.get_user_by_name(username_old)
-		model.update_username(username_old, username_new)
-		user_after_update = model.get_user_by_name(username_new)
+		user_before_update = repository.get_user_by_name(username_old)
+		repository.update_username(username_old, username_new)
+		user_after_update = repository.get_user_by_name(username_new)
 		assert (
-				model.get_user_by_name("user1") is None and
+				repository.get_user_by_name("user1") is None and
 				user_before_update["username"] == username_old and
 				user_after_update["username"] == username_new and
-				model.updated is 1
+				repository.updated is 1
 		)
 
 		# DELETE
-		model.delete_user(username_new)
-		use_after_delete = model.get_user_by_name(username_old)
-		assert use_after_delete is None and model.deleted is 1
+		repository.delete_user(username_new)
+		use_after_delete = repository.get_user_by_name(username_old)
+		assert use_after_delete is None and repository.deleted is 1
 
-		model.delete_users()
-		users = model.list_users()
-		assert len(users) is 0 and model.deleted is 5
+		repository.delete_users()
+		users = repository.list_users()
+		assert len(users) is 0 and repository.deleted is 5
