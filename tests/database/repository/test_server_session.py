317a698ce44fcf3b1123be04217699d6f8a39ee4
Koubae <58447627+Koubae@users.noreply.github.com>
2023-05-01 18:48:58 +0200

    pytest --> added get_session_or_create tests

--- tests/database/repository/test_server_session.py
+++ tests/database/repository/test_server_session.py
@@ -1,17 +1,19 @@
 import pytest
 import time
+from datetime import datetime
 
+from chatbox.app.constants import DATETIME_DEFAULT
 from chatbox.app.database.repository.server_session import ServerSessionRepository, ServerSessionModel
 from tests.conftest import BaseRunner
 
 
-class ServerSessionRepositoryRepository(BaseRunner):
-	def _create_session(self, repository: ServerSessionRepository):
+class TestServerSessionRepositoryRepository(BaseRunner):
+	@staticmethod
+	def _create_session(repository: ServerSessionRepository):
 		session_id = ServerSessionModel.create_session_id()
 		data = {"key1": "val1", "key2": ["value" for _ in range(10)], "key3": {"object": "helloworld"}}
 		server_session: ServerSessionModel = repository.create({"session_id": session_id, "data": data})
 		return session_id, server_session
-
 
 	@pytest.mark.repo_server_session
 	@pytest.mark.repository
@@ -21,7 +23,7 @@ class ServerSessionRepositoryRepository(BaseRunner):
 
 		session_id, server_session = self._create_session(repository)
 
-		assert server_session.id is 1 and server_session.session_id == session_id and server_session.data.__class__ is dict
+		assert server_session.session_id == session_id and server_session.data.__class__ is dict
 
 	@pytest.mark.repo_server_session
 	@pytest.mark.repository
@@ -42,7 +44,7 @@ class ServerSessionRepositoryRepository(BaseRunner):
 	def test_get_many(self):
 		repository = ServerSessionRepository(self.db)
 
-		sessions_ids = [ServerSessionModel.create_session_id() for i in range(10)]
+		sessions_ids = [ServerSessionModel.create_session_id() for _ in range(10)]
 		for session_id in sessions_ids:
 			repository.create({"session_id": session_id, "data":  {"key1": "val"}})
 
@@ -82,3 +84,43 @@ class ServerSessionRepositoryRepository(BaseRunner):
 		session_delete: None = repository.get(server_session.id)
 
 		assert deleted and session_delete is None
+
+	@pytest.mark.repo_server_session
+	@pytest.mark.repository
+	@pytest.mark.database
+	def test_get_session_or_create(self):
+		repository = ServerSessionRepository(self.db)
+
+		server_session: ServerSessionModel = repository.get_session_or_create()
+
+		assert server_session is not None and server_session.__class__ is ServerSessionModel
+
+	@pytest.mark.repo_server_session
+	@pytest.mark.repository
+	@pytest.mark.database
+	def test_get_session_or_create_will_create_new_session_if_none_are_found(self):
+		repository = ServerSessionRepository(self.db)
+
+		# let's delete manually all session
+		repository.db.delete("DELETE FROM server_session")  # noqa
+		server_session: ServerSessionModel = repository.get_session_or_create()
+
+		assert server_session is not None and server_session.__class__ is ServerSessionModel
+
+	@pytest.mark.repo_server_session
+	@pytest.mark.repository
+	@pytest.mark.database
+	def test_get_session_or_create_creates_new_session_when_previous_expires(self):
+		repository = ServerSessionRepository(self.db)
+
+		server_session: ServerSessionModel = repository.get_session_or_create()
+
+		# Change manually the modified session
+		session_updated = repository.update(server_session.id, {'modified': datetime.strftime(datetime(1991, 5, 11), DATETIME_DEFAULT)})
+
+		server_session_new: ServerSessionModel = repository.get_session_or_create()
+		assert (
+				server_session.id == session_updated.id and
+				server_session_new.id is not server_session.id and
+				server_session_new.id == server_session.id + 1
+		)
