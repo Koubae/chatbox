951896a50f66e46a0f803036e373ea5bfde7a3e1
Koubae <58447627+Koubae@users.noreply.github.com>
2023-05-24 05:47:40 +0200

    GUI : added NOtImplementedError to some functions

--- chatbox/app/core/components/client/ui/gui/core/connector.py
+++ chatbox/app/core/components/client/ui/gui/core/connector.py
@@ -210,80 +210,16 @@ class ChatConnector:
 		return ""
 
 	def print_table(self, data) -> None:
-			columns = list(data[0].keys() if data else [])
-
-			table = [columns] + [[str(row.get(col, '')) for col in columns] for row in data]
-			column_size = [max(map(len, col)) for col in zip(*table)]
-
-			format_string = ' | '.join(["{{:<{}}}".format(i) for i in column_size])
-
-			header_separator = ['-' * i for i in column_size]
-			table.insert(1, header_separator)  # Header Separators line
-			for item in table:
-				self.message_echo(format_string.format(*item))
+		raise NotImplementedError("TODO: Requires implementation")
 
 	def print_box(self, items: list[dict]) -> None:
-		spacing = 30
-
-		self.message_echo("")
-		for item in items:
-			list_objects: list[tuple[str, t.Iterable[dict]]] = []
-			name = item.get("name", None)
-			if name:
-				self.message_echo("-" * spacing)
-				self.message_echo(f"{name:>{int(spacing / 2)}}")
-				self.message_echo("-" * spacing)
-			self.message_echo("")
-			for key, value in item.items():
-				if isinstance(value, (list, tuple, set)):
-					list_objects.append((key, value))
-					continue
-				self.message_echo(f"- {key}: {value}")
-
-			for key, value in list_objects:
-				self.message_echo(f"- {key}: ")
-				for entry in value:
-					data = [str(value) for value in entry.values()]
-					self.message_echo(f"\t- {', '.join(data)}")
-
-			self.message_echo("\n" + "=" * (spacing * 2) + "\n")
+		raise NotImplementedError("TODO: Requires implementation")
 
 	def _display_table_csv_type(self, _type: str, payload: ServerMessageModel, add_members: bool = False) -> None:
-		if not payload.body:
-			self.message_echo(f"No {_type} to display")
-			return
-
-		try:
-			items = BaseController.json_decode(payload.body)
-		except BaseControllerException as error:
-			self.message_echo(f"Error while decoding Server response, reason : {error}")
-		else:
-			if not items:
-				self.message_echo(f"No {_type} to display")
-				return
-			self.message_echo(f"These are {_type}:\n\n")
-			if add_members:
-				for item in items:
-					item['members'] = item['members'][:10]  # Hack in terminal ui, print_table don't look nice when there is too much data in one cell
-			self.print_table(items)
-			self.message_echo("\n")
+		raise NotImplementedError("TODO: Requires implementation")
 
 	def _display_table_box_type(self, _type: str, payload: ServerMessageModel) -> None:
-		if not payload.body:
-			self.message_echo(f"No {_type} to display")
-			return
-
-		try:
-			items = BaseController.json_decode(payload.body)
-		except BaseControllerException as error:
-			self.message_echo(f"Error while decoding Server response, reason : {error}")
-		else:
-			if not items:
-				self.message_echo(f"No {_type} to display")
-				return
-			self.message_echo(f"These are {_type}:\n\n")
-			self.print_box(items)
-			self.message_echo("\n")
+		raise NotImplementedError("TODO: Requires implementation")
 
 	def stop_waiting(self):
 		self.gui.window.chat_ui.message_submit()
